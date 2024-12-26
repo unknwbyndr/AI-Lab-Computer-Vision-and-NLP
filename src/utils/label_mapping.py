0cfa5300bd2ae3b9ba30cde0de15dd89082bb690
unknwbyndr <122069659+unknwbyndr@users.noreply.github.com>
2024-12-26 23:04:05 +0100

    Create label_mapping.py

---
+++ src/utils/label_mapping.py
@@ -0,0 +1,10 @@
+# Dictionary mapping numeric labels to emotion categories
+label_dict = {
+    0: 'Angry',     
+    1: 'Disgust',   
+    2: 'Fear',      
+    3: 'Happy',     
+    4: 'Neutral',   
+    5: 'Sad',       
+    6: 'Surprise'   
+}
