9cb0f4f8043160c1317ffcdff609d14c730c0894
unknwbyndr <122069659+unknwbyndr@users.noreply.github.com>
2024-12-26 22:51:28 +0100

    Create data_paths.py

---
+++ src/data/data_paths.py
@@ -0,0 +1,5 @@
+# Define the path to the directory containing the training images
+train_dir = "/kaggle/input/fer2013/train"
+
+# Define the path to the directory containing the testing images
+test_dir = "/kaggle/input/fer2013/test"
